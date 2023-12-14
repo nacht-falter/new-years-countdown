1753a26fcab719b0eb9518145dd4dd660a9bc8d9
Dinis Machado <137174511+DinisMachado87@users.noreply.github.com>
2023-12-14 18:21:42 +0100

    Dinis (#26)

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 31, 2023 00:00:00").getTime();
+
+// Update the countdown every 1 second
+var countdownInterval = setInterval(function() {
+
+    // Get the current date and time
+    var now = new Date().getTime();
+
+    // Calculate the remaining time
+    var distance = countDownDate - now;
+
+    // Calculate days, hours, minutes, and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Update the HTML of the countdown elements
+    document.getElementById("days").innerHTML = days + "d";
+    document.getElementById("hours").innerHTML = hours + "h";
+    document.getElementById("minutes").innerHTML = minutes + "m";
+    document.getElementById("seconds").innerHTML = seconds + "s";
+
+    // If the countdown is over, display a message
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("days").innerHTML = "Happy new year!";
+        document.getElementById("hours").innerHTML = "";
+        document.getElementById("minutes").innerHTML = "";
+        document.getElementById("seconds").innerHTML = "";
+    }
+}, 1000);
