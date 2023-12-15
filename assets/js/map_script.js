695acb235ebfe72c15b05c83d6bd2e69b34c40db
Yannick Ferenczi <yannick.ferenczi@outlook.com>
2023-12-15 22:36:27 +0100

    add countdown to the map

--- assets/js/map_script.js
+++ assets/js/map_script.js
@@ -4,6 +4,8 @@ const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/yannickferenczi/clq6eztad001001pka7aa4274",
     zoom: 1,
+    minZoom: 1,
+    maxZoom: 2,
 });
 
 const geojson = {
@@ -12,6 +14,7 @@ const geojson = {
         {
             type: "Feature",
             properties: {
+                id: 1,
                 name: "Rio de Janeiro",
                 timezone: -2,
                 nye_countdown: "COUNTDOWN !"
@@ -27,6 +30,7 @@ const geojson = {
         {
             type: "Feature",
             properties: {
+                id: 2,
                 name: "Los Angeles",
                 timezone: -2,
                 nye_countdown: "COUNTDOWN !"
@@ -38,9 +42,27 @@ const geojson = {
                 ],
                 type: "Point"
             }
+        },
+        {
+            type: "Feature",
+            properties: {
+                id: 3,
+                name: "New York",
+                timezone: -5,
+                nye_countdown: "COUNTDOWN !"
+            },
+            geometry: {
+                coordinates: [
+                    -73.977155,
+                    40.638384
+                ],
+                type: "Point"
+            }
         }
     ]
 };
+
+var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
 
 for (const feature of geojson.features) {
     // Create a container for every marker on the map
@@ -52,11 +74,28 @@ for (const feature of geojson.features) {
     markerElement.className = 'marker';
     // Add the properties to the marker container
     const propertiesElement = document.createElement("div");
-    propertiesElement.className = 'marker-properties';
     markerContainer.appendChild(propertiesElement);
-    propertiesElement.innerHTML += `<h4 class="centered">${feature.properties.name}</h4>`;
-    propertiesElement.innerHTML += `<p class="centered">${feature.properties.nye_countdown}</p>`;
+    propertiesElement.setAttribute("id", `marker-properties-${feature.properties.id}`);
+    // Create countdown
+    let x = setInterval(function() {
+        let now = new Date().getTime();
+        let timeDifference = countDownDate - now;
 
+        let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + feature.properties.timezone;
+        let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        
+        let propertiesElement = document.getElementById(`marker-properties-${feature.properties.id}`);
+        propertiesElement.innerHTML = `<h4 class="centered">${feature.properties.name}</h4>`;
+        propertiesElement.innerHTML += `<p class="centered">${days}d ${hours}h ${minutes}m ${seconds}s</p>`;
+
+        if (timeDifference <= 0) {
+            clearInterval(x);
+            propertiesElement.innerHTML = `<h4 class="centered">${feature.properties.name}</h4>`;
+            propertiesElement.innerHTML += `<p class="centered">Happy New Year !</p>`;
+        }
+
+    }, 1000);
     new mapboxgl.Marker(markerContainer).setLngLat(feature.geometry.coordinates).addTo(map);
 };
-
